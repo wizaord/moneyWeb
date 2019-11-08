80f2e6cd4f84c4add175e6655ed3a0fd04a23e86
Cédric Mouilleron <mouilleron.cedric@gmail.com>
2019-11-08 18:32:56 +0100

    Initialisation du projet api-administration

---
+++ backend/api-administration/src/main/java/com/wizaord/moneyweb/apiadmin/ApiAdministration.java
@@ -0,0 +1,12 @@
+package com.wizaord.moneyweb.apiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdministration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAdministration.class, args);
+    }
+}

694dab245d57329e23ee5036ff2c8ccf629aa08e
cedric.mouilleron <cedric.mouilleron@orange.com>
2020-01-21 07:26:36 +0100

    {AccountManage} Ajout d'un test pour s'assurer que les comptes filtrés fonctionnent

--- frontend/src/app/domain/account/Account.ts
+++ frontend/src/app/domain/account/Account.ts
@@ -5,4 +5,18 @@ export class Account {
   dateCreate: Date;
   isOpened: boolean;
   owners: string[];
+
+  constructor(id: string,
+              accountName: string,
+              bankName: string,
+              openDate: Date,
+              isOpened: boolean,
+  ) {
+    this.id = id;
+    this.accountName = accountName;
+    this.bankName = bankName;
+    this.dateCreate = openDate;
+    this.isOpened = isOpened;
+    this.owners = [];
+  }
 }
--- frontend/src/app/services/account.service.spec.ts
+++ frontend/src/app/services/account.service.spec.ts
@@ -1,13 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Account } from '../domain/account/Account';
 
 describe('AccountService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let injector: TestBed;
+  let service: AccountService;
+  let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    const service: AccountService = TestBed.get(AccountService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AccountService]
+    });
+    injector = getTestBed();
+    service = injector.get(AccountService);
+    httpMock = injector.get(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('get Accounts opened', () => {
+
+    it('Closed Accounts are filtered', () => {
+      // given
+      const accountOpened = new Account('id', 'accountName', 'bankName', new Date(), true);
+      const accountClosed = new Account('id2', 'accountName', 'bankName', new Date(), false);
+
+      // when
+      service.getOpenedAccounts().subscribe(
+        accounts => {
+          expect(accounts).toBeDefined();
+          expect(accounts.length).toBe(1);
+          expect(accounts[0].id).toBe('id');
+        },
+        error => fail()
+      );
+
+      const req = httpMock.expectOne(`${service.API_URL}`);
+      expect(req.request.method).toBe('GET');
+      req.flush(Array(accountOpened, accountClosed));
+    });
+  });
 });
